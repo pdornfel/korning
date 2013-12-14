a4575a1b32c79171bf82ee1608aa876d4c7bad4a
Paul Dornfeld <pdornfel@gmail.com>
2013-12-14 13:30:46 -0500

    -added employees to employee table under first name.
    -added employee_id in sales table and populated it with id from employee table

---
+++ db/migrate/20131214175903_add_employee_to_employees.rb
@@ -0,0 +1,26 @@
+class AddEmployeeToEmployees < ActiveRecord::Migration
+
+  def up
+
+    add_column :sales, :employee_id, :integer
+
+    Sale.all.each do |sale|
+      employee = Employee.where(first_name: sale.employee).first
+      if employee.nil?
+        employee = Employee.create!(first_name: sale.employee)
+
+        puts "#{employee.first_name} just added to Employees table"
+
+      end
+
+      sale.employee_id = employee.id
+      sale.save!
+
+  end
+
+  end
+
+  def down
+    remove_column :sales, :employee_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214174721) do
+ActiveRecord::Schema.define(version: 20131214175903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20131214174721) do
     t.string   "invoice_frequency"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "employee_id"
   end
 
 end
