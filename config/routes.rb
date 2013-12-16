cc615588c68b1ef87f1fb3fadb65590678f866d3
Paul Dornfeld <pdornfel@gmail.com>
2013-12-16 15:02:51 -0500

    added controller, views, and models for products and customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Korning::Application.routes.draw do
 
-  resources :employees, :sales
+  resources :employees, :sales, :customers
+
+  resources :products, only: [:index, :show]
 
   # resources :employees, only: [:index, :show]
   # resources :sales, only: [:index, :show]
